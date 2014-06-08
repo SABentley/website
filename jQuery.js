840fa6bae3073dbff7e7f30296468bbb28345fea
Sheldon Bentley <sheldonanibentley@gmail.com>
2014-06-07 22:38:02 -0700

    size changes and added hover effects to buttons

--- jQuery.js
+++ jQuery.js
@@ -1,11 +1,29 @@
 window.onload = function() 
 {
+	var loaded = false;
+
 	$('#module').fadeTo(750, 1, function() 
 	{
 		$('#buttonList').fadeTo(750, 1, function() 
 		{
-			//done
+			loaded = true;
 		});
     });
+
+
+	$(".button").mouseenter(function() 
+	{
+		$(this).css("color", "#73AF2B");
+		$(this).css("cursor", "pointer");
+	});
 	
+	$(".button").mouseleave(function()
+	{
+		$(this).css("color", "#E9E017");
+	});
+
 };
+
+
+
+
